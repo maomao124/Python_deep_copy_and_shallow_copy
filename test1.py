41db12e2ea3ceadb2ebe24bfb22f602e7e987983
毛 <1296193245@qq.com>
2022-03-23 13:33:22 +0800

    Python_deep_copy_and_shallow_copy

---
+++ test1.py
@@ -0,0 +1,49 @@
+"""
+ * Project name(项目名称)：Python深拷贝和浅拷贝
+ * Package(包名): 
+ * File(文件名): test1
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/23 
+ * Time(创建时间)： 13:08
+ * Version(版本): 1.0
+ * Description(描述)： 
+ """
+# 统计时间需要用到 time 模块中的函数，了解即可
+import time
+
+
+def find_unique_price_using_list(products):
+    unique_price_list = []
+    for _, price in products:  # A
+        if price not in unique_price_list:  # B
+            unique_price_list.append(price)
+    return len(unique_price_list)
+
+
+if __name__ == '__main__':
+    id = [x for x in range(0, 10000)]
+    price = [x for x in range(20000, 30000)]
+    products = list(zip(id, price))
+    # 计算列表版本的时间
+    start_using_list = time.perf_counter()
+    find_unique_price_using_list(products)
+    end_using_list = time.perf_counter()
+    print("time elapse using list: {}".format(end_using_list - start_using_list))
+
+
+# 使用集合完成同样的工作
+def find_unique_price_using_set(products):
+    unique_price_set = set()
+    for _, price in products:
+        unique_price_set.add(price)
+    return len(unique_price_set)
+
+
+if __name__ == '__main__':
+    # 计算集合版本的时间
+    start_using_set = time.perf_counter()
+    find_unique_price_using_set(products)
+    end_using_set = time.perf_counter()
+    print("time elapse using set: {}".format(end_using_set - start_using_set))
---
+++ test2.py
@@ -0,0 +1,79 @@
+"""
+ * Project name(项目名称)：Python深拷贝和浅拷贝
+ * Package(包名): 
+ * File(文件名): test2
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/23 
+ * Time(创建时间)： 13:20
+ * Version(版本): 1.0
+ * Description(描述)： 浅拷贝，指的是重新分配一块内存，创建一个新的对象，但里面的元素是原对象中各个子对象的引用
+ 深拷贝，是指重新分配一块内存，创建一个新的对象，并且将原对象中的元素，以递归的方式，通过创建新的子对象拷贝到新对象中
+ """
+import copy
+
+if __name__ == '__main__':
+    # 浅拷贝
+    list1 = [1, 2, 3]
+    list2 = list(list1)
+    print(list2)
+    print("list1==list2 ?", list1 == list2)
+    print("list1 is list2 ?", list1 is list2)
+    set1 = set([1, 2, 3])
+    set2 = set(set1)
+    print(set2)
+    print("set1==set2 ?", set1 == set2)
+    print("set1 is set2 ?", set1 is set2)
+
+    list1 = [1, 2, 3]
+    list2 = list1[:]
+    print(list2)
+    print("list1 == list2 ?", list1 == list2)
+    print("list1 is list2 ?", list1 is list2)
+
+    list1 = [1, 2, 3]
+    list2 = copy.copy(list1)
+    print(list2)
+    print("list1 == list2 ?", list1 == list2)
+    print("list1 is list2 ?", list1 is list2)
+
+    # 对于元组，使用 tuple() 或者切片操作符 ':' 不会创建一份浅拷贝，相反它会返回一个指向相同元组的引用
+    tuple1 = (1, 2, 3)
+    tuple2 = tuple(tuple1)
+    print(tuple2)
+    print("tuple1 == tuple2 ?", tuple1 == tuple2)
+    print("tuple1 is tuple2 ?", tuple1 is tuple2)
+
+    list1 = [[1, 2], (30, 40)]
+    list2 = list(list1)
+    list1.append(100)
+    print("list1:", list1)
+    print("list2:", list2)
+    list1[0].append(3)
+    print("list1:", list1)
+    print("list2:", list2)
+    list1[1] += (50, 60)
+    print("list1:", list1)
+    print("list2:", list2)
+
+    # 深拷贝
+    print()
+    list1 = [[1, 2], (30, 40)]
+    # 对任意 Python 对象的深拷贝操作。
+    list2 = copy.deepcopy(list1)
+    list1.append(100)
+    print("list1:", list1)
+    print("list2:", list2)
+    list1[0].append(3)
+    print("list1:", list1)
+    print("list2:", list2)
+    list1[1] += (50, 60)
+    print("list1:", list1)
+    print("list2:", list2)
+
+    list1 = [1]
+    list1.append(list1)
+    print(list1)
+    list2 = copy.deepcopy(list1)
+    print(list2)
